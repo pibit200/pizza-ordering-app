6d93e5480b065596c0ab028522215daff5fed470
Prathmesh <pratbhar83@gmail.com>
2023-06-24 15:18:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const ejs=require("ejs");
+const expressLayout=require("express-ejs-layouts");
+const path=require("path");
+const app=express();
+const PORT=process.env.PORT || 3000;
+
+
+
+
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+
+
+app.use(expressLayout);
+app.set('views',path.join(__dirname,'/resources/views'));
+app.set("view engine","ejs");
+
+
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log("started at port 3000");
+})
